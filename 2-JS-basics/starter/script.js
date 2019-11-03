266362a7ebd9e0cbe2e893c4d7bba7ad6f6255ea
robbie <robbiekruszynski@gmail.com>
2019-11-03 15:15:20 -0800

    add conditional work

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,6 +1,53 @@
 const firstName = "Robbie";
-const myAge = 30;
 console.log(firstName);
-console.log(myAge);
+
 const lastName = "Kruzynski";
 console.log(lastName);
+
+let fullAge = true;
+const myAge = 30;
+console.log(myAge);
+console.log(fullAge);
+console.log(firstName + " " + lastName + " " + myAge + " " + fullAge);
+
+let job, isMarried;
+job = "dev";
+isMarried = true;
+
+console.log(
+  firstName +
+    " " +
+    lastName +
+    " " +
+    "my age is" +
+    " " +
+    myAge +
+    " " +
+    "is that true?" +
+    " " +
+    fullAge +
+    "my job is a" +
+    " " +
+    job +
+    " " +
+    "and I am married" +
+    " " +
+    isMarried
+);
+
+let year, yearJohn, yearMark;
+year = 2019;
+yearJohn = year - 30;
+yearMark = year - 10;
+
+console.log(yearJohn);
+
+console.log(year * 2);
+
+let now, yearKate, yearRobbie;
+now = 2019;
+yearKate = 38;
+yearRobbie = 30;
+
+let kateOlder = yearKate > yearRobbie;
+console.log(kateOlder);
